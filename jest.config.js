fa19b693804ec1b7e6faa54c320c7e8033437a94
Adam Bergman <adam@fransvilhelm.com>
2019-02-12 14:16:56 +0100

    test: Add setups

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    'react-testing-library/cleanup-after-each',
+    'jest-dom/extend-expect',
+  ],
 };
